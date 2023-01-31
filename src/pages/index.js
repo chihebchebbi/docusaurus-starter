0d33cd600b7fa6bc9b272991b1df669d565df917
Chiheb Chebbi <chiheb-chebbi@outlook.fr>
2023-01-30 23:22:52 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            EXPLORE CONTENT ⏱️
+            EXPLORE CONTENT
           </Link>
         </div>
       </div>
